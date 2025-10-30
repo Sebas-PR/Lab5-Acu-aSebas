748d31be3b85baeb02070761174b02278cc406a2
Sebas-PR <porrasrodriguez103050@gmail.com>
2025-10-29 20:32:20 -0600

    Calculo de area del cuadrado

--- Lab5-SebasAcuña/Main.cpp
+++ Lab5-SebasAcuña/Main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "Shape.h"
+#include "Square.h"
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    Shape* soyUnaFigura = new Square(2.0);
+
+
+	double resultado = soyUnaFigura->calculateArea();
+
+
+    std::cout << "Hola, el area de un cuadrado de base 2 es!\n";
+    std::cout << resultado;
 }
