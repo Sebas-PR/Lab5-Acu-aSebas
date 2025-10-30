9209f2c371bc13209a34f2c168ffc2d8f27f1c17
Daniel Acuña <acunamoradaniel10@gmail.com>
2025-10-29 20:57:13 -0600

    termine el circulo

--- Lab5-SebasAcuña/Main.cpp
+++ Lab5-SebasAcuña/Main.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 #include "Shape.h"
 #include "Square.h"
+#include "Circle.h"
 
 
 int main()
 {
 
     Shape* soyUnaFigura = new Square(2.0);
+    Shape* soyotraFigura = new Circle(2.0);
 
 
-	double resultado = soyUnaFigura->calculateArea();
+	double resultadocuadro = soyUnaFigura->calculateArea();
+    double resultacircle= soyotraFigura->calculateArea();
 
 
     std::cout << "Hola, el area de un cuadrado de base 2 es!\n";
-    std::cout << resultado;
+    std::cout << resultadocuadro;
+
+    std::cout << "El area de un circulo con radio 2 es \n";
+    std::cout << resultacircle;
 }
+
